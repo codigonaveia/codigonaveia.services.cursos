80ce23ddd2048ea65865d89086e9e99b8db1483c
codigonaveia <lemosinfotec@gmail.com>
2022-10-02 11:28:25 -0300

    Adicionado Interface Cursos

---
+++ codigonaveia.services.cursos.WebApplication/Reposity/ICursos.cs
@@ -0,0 +1,9 @@
+﻿using codigonaveia.services.cursos.WebApplication.Models;
+
+namespace codigonaveia.services.cursos.WebApplication.Reposity
+{
+    public interface ICursos
+    {
+        Task Registrar(CursosViewModel mod);
+    }
+}
